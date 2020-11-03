f7fdc8cdb91ffc2cbd116bbab8134335026f2ca1
Jesse Smith <cappellacoding@gmail.com>
2020-11-02 20:43:02 -0500

    added name to binding model

--- WebAPI/Models/AccountBindingModels.cs
+++ WebAPI/Models/AccountBindingModels.cs
@@ -35,6 +35,10 @@ namespace WebAPI.Models
     public class RegisterBindingModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Name { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
