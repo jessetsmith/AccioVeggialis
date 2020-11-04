cb1b9ba68d4920273155763530654402b47d92ef
jcharfauros <julia.charfauros@gmail.com>
2020-11-04 17:40:59 -0500

    vegCreate, vegEdit, vegListItem

---
+++ Models/VegCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AccioVegialis.Models
+{
+    public class VegCreate
+    {
+        [Required]
+        [MinLength(5, ErrorMessage = "Please enter at least 5 characters for the Title.")]
+        [MaxLength(50, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+        [MaxLength(1000)]
+        public string Description { get; set; }
+    }
+}
---
+++ Models/VegEdit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccioVegialis.Models
+{
+    public class VegEdit
+    {
+        public int VegetableID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
